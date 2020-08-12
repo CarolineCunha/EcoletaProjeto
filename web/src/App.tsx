b9d5b47d5595b27a7c8e5596fc03c8f8ab081ef7
CarolineCunha <caroline.cunha.b@hotmail.com>
2020-08-11 22:38:30 -0300

    Initial commit

---
+++ web/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Routes from './routes';
+
+//jsx : sintaxe de xml dentro do javascript
+function App() {
+  return (
+
+      <Routes/>
+  );
+}
+
+export default App;
